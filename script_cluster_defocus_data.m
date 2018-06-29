64a68c8aa658921b82892a8eaa80e422a97abaf9
simonk86 <simonkey@gmail.com>
2018-06-29 12:47:59 -0400

    fixed running errors on cluster

--- script_cluster_defocus_data.m
+++ script_cluster_defocus_data.m
@@ -4,45 +4,67 @@ loadQuivviaDependencies();
 clc;clearvars;close all
 labpath = labPath();
 
-% uprFOVs = [...
-%     48,2018,05,15,4,1,3;...
-%     51,2018,05,31,1,1,2;...
-%     51,2018,05,31,1,1,3;...
-%     52,2018,05,31,1,1,1;...
-%     52,2018,05,31,1,1,2]; %ivq# ,y,m,d, session,slice,fov,
-% basepath = fullfile(labpath, 'Labmembers', 'Yoav Adam', 'Data', 'In Vivo', 'Hippocampus');% 'X:\Lab\Labmembers\Yoav Adam\Data\In Vivo\Hippocampus';
-
 uprFOVs = [...
     48,2018,05,15,4,1,3;...
-    %51,2018,05,31,1,1,2;...
-    %51,2018,05,31,1,1,3;...
+    51,2018,05,31,1,1,2;...
+    51,2018,05,31,1,1,3;...
     52,2018,05,31,1,1,1;...
     52,2018,05,31,1,1,2]; %ivq# ,y,m,d, session,slice,fov,
-basepath = fullfile(labpath, 'Labmembers', 'Simon Kheifets', 'Signal Extraction', 'Test Data');% 'X:\Lab\Labmembers\Yoav Adam\Data\In Vivo\Hippocampus';
+basepath = fullfile(labpath, 'Labmembers', 'Yoav Adam', 'Data', 'In Vivo', 'Hippocampus');% 'X:\Lab\Labmembers\Yoav Adam\Data\In Vivo\Hippocampus';
 
-outputpath = fullfile(cd, 'out');
-outputstring = 'test_defocus';
+% uprFOVs = [...
+%     48,2018,05,15,4,1,3;...
+%     %51,2018,05,31,1,1,2;...
+%     %51,2018,05,31,1,1,3;...
+%     52,2018,05,31,1,1,1;...
+%     52,2018,05,31,1,1,2]; %ivq# ,y,m,d, session,slice,fov,
+% basepath = fullfile(labpath, 'Labmembers', 'Simon Kheifets', 'Signal Extraction', 'Test Data');% 'X:\Lab\Labmembers\Yoav Adam\Data\In Vivo\Hippocampus';
+
+outputpath = '/n/regal/cohen_lab/skheifets/qvout/test1';
+outputstring = 'test_defocus1';
 fovpaths = fovPath(uprFOVs);
 datfile = 'movReg.bin';
 
-n=1;
 pathsin = cellfun(@(x) fullfile(basepath,x),fovpaths,'UniformOutput',false);
 regalbasepath = '/n/regal/cohen_lab/skheifets/tempuprdata/';
 %[regalpaths, status, msg] = copyToRegal(basepath,regalbasepath,fovpaths);
 
+%copy and count files
 
-% for i = 1:length(fovpaths)
-for i = 1:1
+n=1;
+for i = 1:length(fovpaths)
     fovpath = fovpaths{i};
     %get folders in each fov path
-    if ~isdir(fovpath)
+    if ~isdir(fullfile(regalbasepath,fovpath))
+        copyToRegal(basepath,regalbasepath,{fovpath});
+    end
+    info = dir(fullfile(regalbasepath,fovpath));
+    folders = {info([info(:).isdir]==1).name};
+    dfolders = folders(~cellfun(@isempty,regexp(folders,'[0-9]{6}_')));
+    for j = 1:length(dfolders)
+        
+        dfolder = dfolders{j};
+        fullpath = fullfile(regalbasepath,fovpath,dfolder);
+        fullpaths{n}= fullpath;
+        n=n+1;
+    end
+end
+numfiles = n-1;
+n=1;
+parpool('local',4);
+tic
+ parfor i = 1:length(fovpaths)
+%for i = 2:2
+    fovpath = fovpaths{i};
+    %get folders in each fov path
+    if ~isdir(fullfile(regalbasepath,fovpath))
         copyToRegal(basepath,regalbasepath,{fovpath});
     end
     info = dir(fullfile(regalbasepath,fovpath));
     folders = {info([info(:).isdir]==1).name};
     dfolders = folders(~cellfun(@isempty,regexp(folders,'[0-9]{6}_')));
     
-    for j = 1:1%length(dfolders)
+    for j = 1:length(dfolders)
         close all
         dfolder = dfolders{j};
         fullpath = fullfile(regalbasepath,fovpath,dfolder);
@@ -66,16 +88,19 @@ for i = 1:1
         fn5 = unqDirFile(savedir,'qAll','pdf');
         append_pdfs(fn5,fns{1},fns{2},fns{3},fns{1});
 
-        fullpaths{n}= fullfile(fullpath,'experimental_parameters.txt');
-        n=n+1;
+        
+ 
         
         fn6 = fullfile(outputpath, [outputstring '_VoltageMovieData']);
         fn7 = fullfile(outputpath, [outputstring '_FilteredVoltageMovie']);
         fn8 = fullfile(outputpath, [outputstring '_PCAs']);
         fn9 = fullfile(outputpath, [outputstring '_ICAs']);
+        fn10 = fullfile(outputpath, [outputstring '_Everything']);
         append_pdfs(fn6,fns{1});
-        append_pdfs(fn6,fns{2});
-        append_pdfs(fn6,fns{3});
-        append_pdfs(fn6,fns{4});
+        append_pdfs(fn7,fns{2});
+        append_pdfs(fn8,fns{3});
+        append_pdfs(fn9,fns{4});
+        append_pdfs(fn10,fns{1},fns{2},fns{3},fns{1});
     end
-end
+ end
+toc
